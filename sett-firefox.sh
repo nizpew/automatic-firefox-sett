e82a0422e19a29b551257811b364e0de83c08449
Nizpew <144165519+nizpew@users.noreply.github.com>
2024-10-09 19:35:55 -0400

    Update sett-firefox.sh

--- sett-firefox.sh
+++ sett-firefox.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 # https://www.youtube.com/watch?v=GVOcElOPs8E&t=110s
 sudo apt-get install -y firefox
-cd ; mkdir arkenfox; cd arkenfox; git clone https://github.com/arkenfox/user.js; git clone https://gitlab.com/trafotin/dotfiles/-/blob/main/user-overrides.js
-
+cd ; mkdir arkenfox; cd arkenfox; git clone https://github.com/arkenfox/user.js; git clone https://gitlab.com/trafotin/dotfiles.git ; cp dotfiles/user-overrides.js .
 # Go to "about:support" in Firefox and click on "Open Directory"
 cd; cp arkenfox/user-overrides.js ~/snap/firefox/common/.mozilla/firefox/*.default ; cp arkenfox/prefsCleaner.sh ~/snap/firefox/common/.mozilla/firefox/*.default; cp arkenfox/user.js ~/snap/firefox/common/.mozilla/firefox/*.default ; cp arkenfox/updater.sh ~/snap/firefox/common/.mozilla/firefox/*.default
 cd ~/snap/firefox/common/.mozilla/firefox/*.default
